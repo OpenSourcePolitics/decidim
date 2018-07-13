8f95ee7dda5359029593fe3d575de958d760d691
armandfardeau <fardeauarmand@gmail.com>
2018-07-13 11:00:18 +0200

    :white_check_mark: Add test for navbar links controller

---
+++ decidim-admin/spec/controllers/navbar_links_controller_spec.rb
@@ -0,0 +1,93 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module Decidim
+  module Admin
+    describe NavbarLinksController, type: :controller do
+      routes { Decidim::Admin::Engine.routes }
+
+      let(:organization) { create :organization }
+      let(:user) { create(:user, :admin, :confirmed, organization: organization) }
+      let(:navbar_link) { create :navbar_link, organization: organization }
+
+      before do
+        sign_in user, scope: :admin
+      end
+
+      describe "GET index" do
+        it "renders the index template" do
+          get :index
+          expect(response).to render_template(:index)
+        end
+      end
+
+      context "when creating a navbar link" do
+        let!(:navbar_link) { create(:navbar_link, organization: organization) }
+
+        it "injects the link to the form" do
+          post :create, params: { navbar_link: navbar_link }
+          expect(assigns(:form).link).to eq(navbar_link.link)
+        end
+
+        it "should calls CreateNavbarLink" do
+          post :create, params: { navbar_link: navbar_link }
+          expect_any_instance_of(CreateNavbarLink).to receive(:call)
+        end
+
+        it "had a flash notice if successfull" do
+          post :create, params: { navbar_link: navbar_link }
+          expect(flash[:notice]).to be_present
+        end
+
+        it "had a flash alert if invalid" do
+          organization = nil
+          post :create, params: { navbar_link: navbar_link }
+          expect(flash[:alert]).to be_present
+        end
+      end
+
+      context "when updating a navbar link" do
+        let!(:navbar_link) { create(:navbar_link, organization: organization) }
+        it "injects the link to the form" do
+          put :update, params: { id: navbar_link.id }.with_indifferent_access
+          expect(assigns(:form).link).to eq(navbar_link.link)
+        end
+
+        it "had a flash notice if successfull" do
+          post :update, params: { navbar_link: navbar_link }
+          expect(flash[:notice]).to be_present
+        end
+
+        it "had a flash alert if invalid" do
+          organization = nil
+          post :update, params: { navbar_link: navbar_link }
+          expect(flash[:alert]).to be_present
+        end
+      end
+
+      context "when no link is given" do
+        it "injects it to the form" do
+          put :update, params: { id: navbar_link.id }.with_indifferent_access
+          expect(assigns(:form).link).to eq(navbar_link.link)
+        end
+
+      end
+
+      context "when a link is given" do
+        it "does not overwrite it" do
+          put :update, params: { id: navbar_link.id, navbar_link: { link: "http://example.org" } }.with_indifferent_access
+          expect(assigns(:form).link).to eq("http://example.org")
+        end
+      end
+
+      context "when a link is destroyed" do
+        let(:navbar_link) { create(:navbar_link, organization: organization) }
+        it "had a flash notice if successfull" do
+          navbar_link.destroy!
+          expect(flash[:notice]).to be_present
+        end
+      end
+    end
+  end
+end
