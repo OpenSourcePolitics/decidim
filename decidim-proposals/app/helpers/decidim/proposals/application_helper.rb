a31491b44639cf6afb7a4b511d88cd4b021271ed
moustachu <git@moustachu.net>
2019-02-24 01:56:06 +0100

    :lipstick: nested menu for category filter

--- decidim-proposals/app/helpers/decidim/proposals/application_helper.rb
+++ decidim-proposals/app/helpers/decidim/proposals/application_helper.rb
@@ -182,6 +182,47 @@ module Decidim
         end
       end
 
+      def categories_nested_dropdown(scope)
+        sorted_main_categories = scope.first_class.includes(:subcategories).sort_by do |category|
+          translated_attribute(category.name, category.participatory_space.organization)
+        end
+
+        template = ""
+        sorted_main_categories.flat_map do |category|
+          if category.subcategories.empty?
+            template += content_tag(:li, class: "category") do
+              link_to(q: merge_query(category_id_eq: category.id)) do
+                translated_attribute(category.name, category.participatory_space.organization)
+              end
+            end
+          else
+            sorted_subcategories = category.subcategories.sort_by do |subcategory|
+              translated_attribute(subcategory.name, subcategory.participatory_space.organization)
+            end
+
+            template += content_tag(:li, class: "is-dropdown-submenu-parent") do
+              nested_template = ""
+              nested_template += link_to(q: merge_query(category_id_eq: category.id)) do
+                translated_attribute(category.name, category.participatory_space.organization)
+              end
+              nested_template += content_tag(:ul, class: "nested vertical menu") do
+                nested_children = ""
+                sorted_subcategories.each do |subcategory|
+                  nested_children += content_tag(:li, class: "subcategory") do
+                    link_to(q: merge_query(category_id_eq: subcategory.id)) do
+                      translated_attribute(subcategory.name, subcategory.participatory_space.organization)
+                    end
+                  end
+                end
+                nested_children.html_safe
+              end
+              nested_template.html_safe
+            end
+          end
+        end
+        template.html_safe
+      end
+
       def type_label_for_all(proposals)
         type_label_toggler = ""
         proposals.each do |proposal|
