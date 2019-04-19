1870ef358ffe075d2f24e57c9c027f91090cb6db
Armand <fardeauarmand@gmail.com>
2019-04-19 17:03:23 +0200

    Update notification

--- decidim-budgets/app/events/decidim/budgets/checkout_order_event.rb
+++ decidim-budgets/app/events/decidim/budgets/checkout_order_event.rb
@@ -3,6 +3,11 @@
 module Decidim
   module Budgets
     class CheckoutOrderEvent < Decidim::Events::SimpleEvent
+      i18n_attributes :participatory_space_title
+
+      def participatory_space_title
+        @resource_name ||= translated_attribute(resource.participatory_space.title)
+      end
     end
   end
 end
