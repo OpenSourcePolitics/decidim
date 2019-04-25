7b1f822a6538dd34a97fe3c2564047efaec04824
Armand <fardeauarmand@gmail.com>
2019-04-25 09:45:47 +0200

    Use progress bar with projects

--- decidim-budgets/app/helpers/decidim/budgets/projects_helper.rb
+++ decidim-budgets/app/helpers/decidim/budgets/projects_helper.rb
@@ -16,6 +16,23 @@ module Decidim
         current_order&.budget_percent.to_f.floor
       end
 
+      # Return a percentage of the current order project from total projects
+      def current_order_project_percent
+        current_order&.project_percent.to_f.floor
+      end
+
+      def current_order_percent_minimum
+        return 100 if component_settings.vote_per_project
+
+        component_settings.vote_threshold_percent
+      end
+
+      def current_order_percent
+        return current_order_budget_percent unless component_settings.vote_per_project
+
+        current_order_project_percent
+      end
+
       # Return true if the current order is checked out
       delegate :checked_out?, to: :current_order, prefix: true, allow_nil: true
 
