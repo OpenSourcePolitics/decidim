8e7255bb1b1b93779be580d6d0e045700c7f16a9
Armand <fardeauarmand@gmail.com>
2019-06-06 14:11:42 +0200

    Ensure parameter is a integer

--- decidim-system/app/controllers/decidim/system/logs_controller.rb
+++ decidim-system/app/controllers/decidim/system/logs_controller.rb
@@ -4,12 +4,20 @@ module Decidim
   module System
     class LogsController < Decidim::System::ApplicationController
       def index
-        lines = params[:lines] || 50
         @logs = `tail -n #{lines} log/#{Rails.env}.log`
       end
 
       def download
         send_file File.open("log/#{Rails.env}.log")
+      end
+
+      private
+
+      def lines
+        lines = params[:lines].to_i
+        return 50 if lines.zero?
+
+        lines
       end
     end
   end
