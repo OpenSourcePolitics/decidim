e4db57dfafa77b128b9aca2138616edcbd748aee
Armand <fardeauarmand@gmail.com>
2019-12-12 17:46:04 +0100

    Fix registration controller spec

--- decidim-core/spec/controllers/registrations_controller_spec.rb
+++ decidim-core/spec/controllers/registrations_controller_spec.rb
@@ -6,6 +6,7 @@ module Decidim
   describe Decidim::Devise::RegistrationsController, type: :controller do
     routes { Decidim::Core::Engine.routes }
 
+    let(:scope) { create(:scope, organization: organization) }
     let(:organization) { create(:organization) }
 
     before do
@@ -17,25 +18,33 @@ module Decidim
       let(:email) { "test@example.org" }
       let(:params) do
         {
-          user: {
-            sign_up_as: "user",
-            name: "User",
-            nickname: "nickname",
-            email: email,
-            password: "rPYWYKQJrXm97b4ytswc",
-            password_confirmation: "rPYWYKQJrXm97b4ytswc",
-            tos_agreement: "1",
-            newsletter: "0"
-          }
+            user: {
+                sign_up_as: "user",
+                name: "User",
+                nickname: "nickname",
+                email: email,
+                password: "rPYWYKQJrXm97b4ytswc",
+                password_confirmation: "rPYWYKQJrXm97b4ytswc",
+                tos_agreement: "1",
+                newsletter: "0",
+                additional_tos: "1",
+                residential_area: scope.id.to_s,
+                work_area: scope.id.to_s,
+                gender: "other",
+                month: "January",
+                year: "1992",
+                underage: "1",
+                statutory_representative_email: "statutory_representative_email@example.org"
+            }
         }
       end
 
       context "when the user created is active for authentication" do
         before do
           expect_any_instance_of(Decidim::User) # rubocop:disable RSpec/AnyInstance
-            .to receive(:active_for_authentication?)
-            .at_least(:once)
-            .and_return(true)
+              .to receive(:active_for_authentication?)
+                      .at_least(:once)
+                      .and_return(true)
           expect(controller).to receive(:sign_up).and_call_original
         end
 
