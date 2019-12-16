74e7dc4385819c49c489d41106ce3580b5b904ae
Armand <fardeauarmand@gmail.com>
2019-12-16 11:55:27 +0100

    Import populate dev

--- decidim-core/app/assets/javascripts/decidim/user_registrations.js.es6
+++ decidim-core/app/assets/javascripts/decidim/user_registrations.js.es6
@@ -1,9 +1,11 @@
 $(() => {
-  const $userRegistrationForm = $("#register-form");
-  const $userGroupFields = $userRegistrationForm.find(".user-group-fields");
-  const inputSelector = 'input[name="user[sign_up_as]"]';
-  const newsletterSelector = 'input[type="checkbox"][name="user[newsletter]"]';
-  const $newsletterModal = $("#sign-up-newsletter-modal");
+  const $userRegistrationForm  = $("#register-form");
+  const $userGroupFields       = $userRegistrationForm.find(".user-group-fields");
+  const inputSelector          = "input[name='user[sign_up_as]']";
+  const newsletterSelector     = "input[type='checkbox'][name='user[newsletter]']";
+  const $newsletterModal       = $("#sign-up-newsletter-modal");
+  const $formStepForwardButton = $(".form-step-forward-button");
+  const $formStepBackButton    = $(".form-step-back-button");
 
   const $underageSelector = $("#underage_registration");
   const $statutoryRepresentativeEmailSelector = $("#statutory_representative_email")
@@ -22,14 +24,19 @@ $(() => {
     } else {
       $userGroupFields.show();
     }
-  }
+  };
+
+  const toggleFromSteps = () => {
+    $("[form-step]").toggle();
+    $("[form-active-step]").toggleClass("step--active");
+  };
 
   const checkNewsletter = (check) => {
     $userRegistrationForm.find(newsletterSelector).prop("checked", check);
     $newsletterModal.data("continue", true);
     $newsletterModal.foundation("close");
     $userRegistrationForm.submit();
-  }
+  };
 
   setGroupFieldsVisibility($userRegistrationForm.find(`${inputSelector}:checked`).val());
 
@@ -51,5 +58,24 @@ $(() => {
 
   $newsletterModal.find(".check-newsletter").on("click", (event) => {
     checkNewsletter($(event.target).data("check"));
+  });
+
+  $formStepForwardButton.on("click", (event) => {
+    event.preventDefault();
+
+    // validate only input elements from step 1
+    $("[form-step='1'] input").each((index, element) => {
+      $userRegistrationForm.foundation("validateInput", $(element));
+    });
+
+    if (!$userRegistrationForm.find("[data-invalid]:visible").length) {
+      toggleFromSteps();
+    }
+  });
+
+  $formStepBackButton.on("click", (event) => {
+    event.preventDefault();
+
+    toggleFromSteps();
   });
 });
--- decidim-core/spec/system/registration_spec.rb
+++ decidim-core/spec/system/registration_spec.rb
@@ -2,23 +2,30 @@
 
 require "spec_helper"
 
-def fill_registration_form
-  fill_in :user_name, with: "Nikola Tesla"
-  fill_in :user_nickname, with: "the-greatest-genius-in-history"
-  fill_in :user_email, with: "nikola.tesla@example.org"
-  fill_in :user_password, with: "sekritpass123"
-  fill_in :user_password_confirmation, with: "sekritpass123"
+def fill_registration_form(params = {})
+  if params[:step] == 1
+    fill_in :user_email, with: "nikola.tesla@example.org"
+    fill_in :user_password, with: "sekritpass123"
+    fill_in :user_password_confirmation, with: "sekritpass123"
+    check("user_tos_agreement")
+    check :user_additional_tos
+  else
+    fill_in :user_name, with: "Nikola Tesla"
+    fill_in :user_nickname, with: "the-greatest-genius-in-history"
+    select translated(scopes.first.name), from: :user_residential_area
+    select translated(scopes.first.name), from: :user_work_area
+    select "Other", from: :user_gender
+    select "September", from: :user_month
+    select "1992", from: :user_year
+    check :underage_registration
+    fill_in :user_statutory_representative_email, with: "milutin.tesla@example.org"
+  end
 end
 
-def fill_registration_metadata
-  select translated(scopes.first.name), from: :user_residential_area
-  select translated(scopes.first.name), from: :user_work_area
-  select "Other", from: :user_gender
-  select "September", from: :user_month
-  select "1992", from: :user_year
-  check :underage_registration
-  fill_in :user_statutory_representative_email, with: "milutin.tesla@example.org"
-  check :user_additional_tos
+def submit_form
+  within("form.new_user") do
+    find("*[type=submit]").click
+  end
 end
 
 describe "Registration", type: :system do
@@ -35,11 +42,33 @@ describe "Registration", type: :system do
     describe "on first sight" do
       it "shows fields empty" do
         expect(page).to have_content("Sign up to participate")
-        expect(page).to have_field("user_name", with: "")
-        expect(page).to have_field("user_nickname", with: "")
         expect(page).to have_field("user_email", with: "")
         expect(page).to have_field("user_password", with: "")
         expect(page).to have_field("user_password_confirmation", with: "")
+      end
+    end
+
+    describe "after clicking in next step" do
+      it "forces user to fill first step attributes" do
+        click_button "Continue"
+
+        expect(page).to have_field("user_email", with: "")
+        expect(page).to have_field("user_password", with: "")
+        expect(page).to have_field("user_password_confirmation", with: "")
+        expect(page).not_to have_field("user_name", with: "")
+        expect(page).not_to have_field("user_nickname", with: "")
+        expect(page).not_to have_field("user_newsletter", checked: false)
+      end
+
+      it "shows fields empty" do
+        fill_registration_form(step: 1)
+        click_button "Continue"
+
+        expect(page).not_to have_field("user_email")
+        expect(page).not_to have_field("user_password")
+        expect(page).not_to have_field("user_password_confirmation")
+        expect(page).to have_field("user_name", with: "")
+        expect(page).to have_field("user_nickname", with: "")
         expect(page).to have_field("user_newsletter", checked: false)
         expect(page).to have_select("user[residential_area]", selected: "Please select")
         expect(page).to have_select("user[work_area]", selected: "Please select")
@@ -53,68 +82,103 @@ describe "Registration", type: :system do
 
   context "when newsletter checkbox is unchecked" do
     it "opens modal on submit" do
-      within "form.new_user" do
-        find("*[type=submit]").click
-      end
+      fill_registration_form(step: 1)
+      click_button "Continue"
+
+      fill_registration_form(step: 2)
+      submit_form
+
       expect(page).to have_css("#sign-up-newsletter-modal", visible: true)
       expect(page).to have_current_path decidim.new_user_registration_path
     end
 
-    it "checks when clicking the checking button" do
-      within "form.new_user" do
-        fill_registration_metadata
-        find("*[type=submit]").click
-      end
-      click_button "Check and continue"
-      expect(page).to have_current_path decidim.new_user_registration_path
-      expect(page).to have_css("#sign-up-newsletter-modal", visible: false)
-      expect(page).to have_field("user_newsletter", checked: true)
+    it "checks when clicking the checking button and user is created" do
+      fill_registration_form(step: 1)
+      click_button "Continue"
+
+      fill_registration_form(step: 2)
+      submit_form
+
+      expect do
+        click_button "Check and continue"
+      end.to change(Decidim::User, :count).by(1)
+
+      expect(page).to have_current_path(decidim.user_complete_registration_path)
+
+      user = Decidim::User.last
+
+      expect(user.newsletter_notifications_at).not_to be_nil
     end
 
     it "submit after modal has been opened and selected an option" do
-      within "form.new_user" do
-        fill_registration_metadata
-        find("*[type=submit]").click
-      end
+      fill_registration_form(step: 1)
+      click_button "Continue"
+      submit_form
+
       click_button "Keep uncheck"
       expect(page).to have_css("#sign-up-newsletter-modal", visible: false)
-      fill_registration_form
-      within "form.new_user" do
-        find("*[type=submit]").click
-      end
-      expect(page).to have_current_path decidim.user_registration_path
+
       expect(page).to have_field("user_newsletter", checked: false)
     end
   end
 
   context "when newsletter checkbox is checked but submit fails" do
     before do
-      fill_registration_form
+      fill_registration_form(step: 1)
+      fill_in :user_password_confirmation, with: "failure"
+      click_button "Continue"
+
+      fill_registration_form(step: 2)
       page.check("user_newsletter")
+      submit_form
+    end
+
+    it "shows all registration fields" do
+      expect(page).to have_content("Sign up to participate")
+      expect(page).to have_field("user_email")
+      expect(page).to have_field("user_password")
+      expect(page).to have_field("user_password_confirmation")
+      expect(page).to have_field("user_name")
+      expect(page).to have_field("user_nickname")
     end
 
     it "keeps the user newsletter checkbox true value" do
-      within "form.new_user" do
-        fill_registration_metadata
-        find("*[type=submit]").click
-      end
       expect(page).to have_current_path decidim.user_registration_path
       expect(page).to have_field("user_newsletter", checked: true)
     end
   end
 
+  context "when newsletter checkbox is checked and registration is successful" do
+    before do
+      fill_registration_form(step: 1)
+      click_button "Continue"
+      fill_registration_form(step: 2)
+      page.check("user_newsletter")
+    end
+
+    it "creates the user" do
+      expect do
+        submit_form
+      end.to change(Decidim::User, :count).by(1)
+
+      expect(page).to have_current_path(decidim.user_complete_registration_path)
+
+      user = Decidim::User.last
+
+      expect(user.newsletter_notifications_at).not_to be_nil
+    end
+  end
+
   context "when registering with additional fields" do
     before do
-      fill_registration_form
-      fill_registration_metadata
+      fill_registration_form(step: 1)
+      click_button "Continue"
+      fill_registration_form(step: 2)
       page.check("user_newsletter")
-      page.check("user_tos_agreement")
     end
 
     it "allows user to register" do
-      within "form.new_user" do
-        find("*[type=submit]").click
-      end
+      submit_form
 
       expect(page).to have_content("Welcome! You have signed up successfully.")
     end
