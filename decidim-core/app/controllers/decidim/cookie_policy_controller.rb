e0760fc2a1b31b4fcf102c99778822426fcf2882
quentinchampenois <quentin.champenois@eemi.com>
2019-12-31 11:40:45 +0100

    Refactor css / html

--- decidim-core/app/controllers/decidim/cookie_policy_controller.rb
+++ decidim-core/app/controllers/decidim/cookie_policy_controller.rb
@@ -17,11 +17,7 @@ module Decidim
     end
 
     def show_terms
-      content = {
-        title: "Let us know you agree to cookies",
-        description: "Un \"cookie\" est une suite d'informations, généralement de petite taille et identifié par un nom, qui peut être transmis à votre navigateur par un site web sur lequel vous vous connectez. Votre navigateur web le conservera pendant une certaine durée, et le renverra au serveur web chaque fois que vous vous y re-connecterez."
-      }
-      render partial: "decidim/cookie_policy/cookie_banner_modal", locals: { content: content }
+      render partial: "decidim/cookie_policy/cookie_banner_modal"
     end
   end
 end
