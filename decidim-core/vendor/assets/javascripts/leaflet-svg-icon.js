1aaa29d7fefff10dea4d3991b8093948834a4a93
moustachu <git@moustachu.net>
2020-01-29 19:14:38 +0100

    update Leaflet assets

--- decidim-core/vendor/assets/javascripts/leaflet-svg-icon.js
+++ decidim-core/vendor/assets/javascripts/leaflet-svg-icon.js
@@ -2,6 +2,9 @@
 //SVG icon for any marker class
 //Ilya Atkin
 //ilya.atkin@unh.edu
+// source : https://github.com/iatkin/leaflet-svgicon/blob/master/svg-icon.js
+// rev : 40df576ac3be707122ae153af00d77757103265f
+
 
 L.DivIcon.SVGIcon = L.DivIcon.extend({
     options: {
@@ -11,7 +14,7 @@ L.DivIcon.SVGIcon = L.DivIcon.extend({
         "circleColor": null, //defaults to color
         "circleOpacity": null, // defaults to opacity
         "circleFillColor": "rgb(255,255,255)",
-        "circleFillOpacity": null, //default to opacity 
+        "circleFillOpacity": null, //default to opacity
         "circleRatio": 0.5,
         "circleWeight": null, //defaults to weight
         "color": "rgb(0,102,255)",
@@ -20,6 +23,7 @@ L.DivIcon.SVGIcon = L.DivIcon.extend({
         "fontColor": "rgb(0, 0, 0)",
         "fontOpacity": "1",
         "fontSize": null, // defaults to iconSize.x/4
+        "fontWeight": "normal",
         "iconAnchor": null, //defaults to [iconSize.x/2, iconSize.y] (point tip)
         "iconSize": L.point(32,48),
         "opacity": 1,
@@ -29,8 +33,15 @@ L.DivIcon.SVGIcon = L.DivIcon.extend({
     initialize: function(options) {
         options = L.Util.setOptions(this, options)
 
+        //iconSize needs to be converted to a Point object if it is not passed as one
+        options.iconSize = L.point(options.iconSize)
+
+        //in addition to setting option dependant defaults, Point-based options are converted to Point objects
         if (!options.circleAnchor) {
             options.circleAnchor = L.point(Number(options.iconSize.x)/2, Number(options.iconSize.x)/2)
+        }
+        else {
+            options.circleAnchor = L.point(options.circleAnchor)
         }
         if (!options.circleColor) {
             options.circleColor = options.color
@@ -44,38 +55,46 @@ L.DivIcon.SVGIcon = L.DivIcon.extend({
         if (!options.circleWeight) {
             options.circleWeight = options.weight
         }
-        if (!options.fillColor) { 
+        if (!options.fillColor) {
             options.fillColor = options.color
         }
         if (!options.fontSize) {
-            options.fontSize = Number(options.iconSize.x/4) 
+            options.fontSize = Number(options.iconSize.x/4)
         }
         if (!options.iconAnchor) {
             options.iconAnchor = L.point(Number(options.iconSize.x)/2, Number(options.iconSize.y))
         }
+        else {
+            options.iconAnchor = L.point(options.iconAnchor)
+        }
         if (!options.popupAnchor) {
             options.popupAnchor = L.point(0, (-0.75)*(options.iconSize.y))
         }
-
-        var path = this._createPath()
-        var circle = this._createCircle()
+        else {
+            options.popupAnchor = L.point(options.popupAnchor)
+        }
 
         options.html = this._createSVG()
     },
     _createCircle: function() {
-        var cx = Number(this.options.circleAnchor.x) 
+        var cx = Number(this.options.circleAnchor.x)
         var cy = Number(this.options.circleAnchor.y)
         var radius = this.options.iconSize.x/2 * Number(this.options.circleRatio)
-        var fill = this.options.circleFillColor.replace("rgb(", "rgba(").replace(")", "," + this.options.circleFillOpacity + ")")
-        var stroke = this.options.circleColor.replace("rgb(", "rgba(").replace(")", "," + this.options.circleOpacity + ")")
+        var fill = this.options.circleFillColor
+        var fillOpacity = this.options.circleFillOpacity
+        var stroke = this.options.circleColor
+        var strokeOpacity = this.options.circleOpacity
         var strokeWidth = this.options.circleWeight
         var className = this.options.className + "-circle"
 
-        var circle = '<circle class="' + className + '" cx="' + cx + '" cy="' + cy + '" r="' + radius + '" fill="' + fill + '" stroke="' + stroke + '" stroke-width="' + strokeWidth + '"/>'
+        var circle = '<circle class="' + className + '" cx="' + cx + '" cy="' + cy + '" r="' + radius +
+            '" fill="' + fill + '" fill-opacity="'+ fillOpacity +
+            '" stroke="' + stroke + '" stroke-opacity=' + strokeOpacity + '" stroke-width="' + strokeWidth + '"/>'
 
         return circle
     },
     _createPathDescription: function() {
+      console.log('_createPathDescription');
         var height = Number(this.options.iconSize.y)
         var width = Number(this.options.iconSize.x)
         var weight = Number(this.options.weight)
@@ -93,11 +112,15 @@ L.DivIcon.SVGIcon = L.DivIcon.extend({
     _createPath: function() {
         var pathDescription = this._createPathDescription()
         var strokeWidth = this.options.weight
-        var stroke = this.options.color.replace("rgb(", "rgba(").replace(")", "," + this.options.opacity + ")")
-        var fill = this.options.fillColor.replace("rgb(", "rgba(").replace(")", "," + this.options.fillOpacity + ")")
+        var stroke = this.options.color
+        var strokeOpacity = this.options.opacity
+        var fill = this.options.fillColor
+        var fillOpacity = this.options.fillOpacity
         var className = this.options.className + "-path"
 
-        var path = '<path class="' + className + '" d="' + pathDescription + '" stroke-width="' + strokeWidth + '" stroke="' + stroke + '" fill="' + fill + '"/>'
+        var path = '<path class="' + className + '" d="' + pathDescription +
+            '" stroke-width="' + strokeWidth + '" stroke="' + stroke + '" stroke-opacity="' + strokeOpacity +
+            '" fill="' + fill + '" fill-opacity="' + fillOpacity + '"/>'
 
         return path
     },
@@ -107,7 +130,7 @@ L.DivIcon.SVGIcon = L.DivIcon.extend({
         var text = this._createText()
         var className = this.options.className + "-svg"
 
-        var style = "width:" + this.options.iconSize.x + "; height:" + this.options.iconSize.y + ";"
+        var style = "width:" + this.options.iconSize.x + "px; height:" + this.options.iconSize.y + "px;"
 
         var svg = '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" class="' + className + '" style="' + style + '">' + path + circle + text + '</svg>'
 
@@ -115,14 +138,15 @@ L.DivIcon.SVGIcon = L.DivIcon.extend({
     },
     _createText: function() {
         var fontSize = this.options.fontSize + "px"
+        var fontWeight = this.options.fontWeight
         var lineHeight = Number(this.options.fontSize)
 
-        var x = Number(this.options.iconSize.x) / 2
-        var y = x + (lineHeight * 0.35) //35% was found experimentally 
+        var x = this.options.circleAnchor.x
+        var y = this.options.circleAnchor.y + (lineHeight * 0.35) //35% was found experimentally
         var circleText = this.options.circleText
         var textColor = this.options.fontColor.replace("rgb(", "rgba(").replace(")", "," + this.options.fontOpacity + ")")
 
-        var text = '<text text-anchor="middle" x="' + x + '" y="' + y + '" style="font-size: ' + fontSize + '" fill="' + textColor + '">' + circleText + '</text>'
+        var text = '<text text-anchor="middle" x="' + x + '" y="' + y + '" style="font-size: ' + fontSize + '; font-weight: ' + fontWeight +'" fill="' + textColor + '">' + circleText + '</text>'
 
         return text
     }
@@ -167,7 +191,7 @@ L.Marker.SVGMarker = L.Marker.extend({
             }
             if (style.iconOptions) {
                 if (style.color) { style.iconOptions.color = style.color }
-                iconOptions = L.Util.setOptions(this.options.icon, style.iconOptions)
+                var iconOptions = L.Util.setOptions(this.options.icon, style.iconOptions)
                 this.setIcon(L.divIcon.svgIcon(iconOptions))
             }
         }
