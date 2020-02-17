bc113035e348609453df2cd8503b6b9894e448bc
quentinchampenois <quentin.champenois@eemi.com>
2020-02-17 17:09:03 +0100

    tests for mailer logo full url presence

--- decidim-core/app/mailers/decidim/application_mailer.rb
+++ decidim-core/app/mailers/decidim/application_mailer.rb
@@ -6,11 +6,19 @@ module Decidim
   class ApplicationMailer < ActionMailer::Base
     include LocalisedMailer
     after_action :set_smtp
+    before_action :set_asset_host
 
     default from: Decidim.config.mailer_sender
     layout "decidim/mailer"
 
     private
+
+    # set_asset_host allows to redefine the host asset if not specified in application configuration
+    def set_asset_host
+      return if Rails.application.config.action_mailer.asset_host.present?
+
+      self.asset_host = ->(_mail) { "http://#{@organization.host}" }
+    end
 
     def set_smtp
       return if @organization.nil? || @organization.smtp_settings.blank?
--- decidim-core/spec/mailers/notification_mailer_spec.rb
+++ decidim-core/spec/mailers/notification_mailer_spec.rb
@@ -45,6 +45,21 @@ module Decidim
         expect(mail.body).to include(event_instance.resource_url)
       end
 
+      context "when organization has a logo" do
+        let(:organization_logo) { Decidim::Dev.test_file("city.jpeg", "image/jpeg") }
+        let(:organization) { create(:organization, logo: organization_logo) }
+        let(:mail) { described_class.event_received(event, event_class_name, resource, user, :follower, extra) }
+
+        it "includes organization logo" do
+          expect(mail.body).to include(organization.logo.medium.url)
+        end
+
+        it "includes organization logo with full link" do
+          expect(mail.body).to include("alt=\"#{organization.name}\"")
+          expect(mail.body).to match(%r{http[s]{0,1}:\/\/#{organization.host}#{organization.logo.medium.url}})
+        end
+      end
+
       context "when the user doesn't have an email" do
         let(:user) { create(:user, :deleted) }
 
