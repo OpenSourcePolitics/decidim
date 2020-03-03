ad6223510c6795a2b16c4e3e9fda332c5ed20095
quentinchampenois <quentin.champenois@eemi.com>
2020-03-03 12:16:43 +0100

    add admin export for json

--- decidim-core/lib/decidim/exporters/json.rb
+++ decidim-core/lib/decidim/exporters/json.rb
@@ -18,6 +18,19 @@ module Decidim
 
         ExportData.new(data, "json")
       end
+
+      # Public: Generates a JSON representation of a collection and a
+      # Serializer.
+      # It injects some informations as author's data which can only be exported by admin
+      #
+      # Returns an ExportData with the export.
+      def admin_export
+        data = ::JSON.pretty_generate(@collection.map do |resource|
+          @serializer.new(resource, false).serialize
+        end)
+
+        ExportData.new(data, "json")
+      end
     end
   end
 end
