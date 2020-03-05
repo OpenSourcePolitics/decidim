258aacd5e26cec622886dbfa6ba68b9fa4a19698
quentinchampenois <quentin.champenois@eemi.com>
2020-03-05 13:28:40 +0100

    edit serializer for admin exports

--- decidim-core/app/serializers/decidim/exporters/serializer.rb
+++ decidim-core/app/serializers/decidim/exporters/serializer.rb
@@ -11,12 +11,15 @@ module Decidim
     # own serializer or this default will be used.
     class Serializer
       attr_reader :resource
+      attr_accessor :admin_extra_fields
 
       # Initializes the serializer with a resource.
       #
       # resource - The Object to serialize.
-      def initialize(resource)
+      # public_scope - Boolean to differentiate open data export and administrator export. By default the export is done by open data.
+      def initialize(resource, public_scope = true)
         @resource = resource
+        @public_scope = public_scope
       end
 
       # Public: Returns a serialized view of the provided resource.
@@ -24,6 +27,19 @@ module Decidim
       # Returns a nested Hash with the fields.
       def serialize
         @resource.to_h
+                 .merge(
+                   options_merge(admin_extra_fields)
+                 )
+      end
+
+      private
+
+      # Private: Returns a Hash with additional fields to export if the export is done by administrator
+      #
+      # Returns a empty hash or Hash with some other fields
+      def options_merge(options = {})
+        return {} unless options.is_a?(Hash) && !@public_scope
+        options
       end
     end
   end
