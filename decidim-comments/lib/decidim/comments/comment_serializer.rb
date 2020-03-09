06894bf9d86e48b5dd70eda439a40157ab77ec8a
quentinchampenois <quentin.champenois@eemi.com>
2020-03-09 15:30:43 +0100

    update comments export column translation

--- decidim-comments/lib/decidim/comments/comment_serializer.rb
+++ decidim-comments/lib/decidim/comments/comment_serializer.rb
@@ -9,23 +9,23 @@ module Decidim
       # Serializes a comment
       def serialize
         {
-          id: resource.id,
-          created_at: resource.created_at,
-          body: resource.body,
-          author: {
-            id: resource.author.id,
-            name: resource.author.name,
-            registration_metadata: resource.author.registration_metadata
+          translated_column_name(:id, "decidim.comments.exports.column_name.comments") => resource.id,
+          translated_column_name(:created_at, "decidim.comments.exports.column_name.comments") => resource.created_at,
+          translated_column_name(:body, "decidim.comments.exports.column_name.comments") => resource.body,
+          translated_column_name(:author, "decidim.comments.exports.column_name.comments.author") => {
+            translated_column_name(:id, "decidim.comments.exports.column_name.comments.author") => resource.author.id,
+            translated_column_name(:name, "decidim.comments.exports.column_name.comments.author") => resource.author.name,
+            translated_column_name(:registration_metadata, "decidim.comments.exports.column_name.comments.author") => resource.author.registration_metadata
           },
-          alignment: resource.alignment,
-          depth: resource.depth,
-          user_group: {
-            id: resource.user_group.try(:id),
-            name: resource.user_group.try(:name) || empty_translatable
+          translated_column_name(:alignment, "decidim.comments.exports.column_name.comments") => resource.alignment,
+          translated_column_name(:depth, "decidim.comments.exports.column_name.comments") => resource.depth,
+          translated_column_name(:user_group, "decidim.comments.exports.column_name.comments.user_group") => {
+            translated_column_name(:id, "decidim.comments.exports.column_name.comments.user_group") => resource.user_group.try(:id),
+            translated_column_name(:name, "decidim.comments.exports.column_name.comments.user_group") => resource.user_group.try(:name) || empty_translatable
           },
-          commentable_id: resource.decidim_commentable_id,
-          commentable_type: resource.decidim_commentable_type,
-          root_commentable_url: root_commentable_url
+          translated_column_name(:commentable_id, "decidim.comments.exports.column_name.comments") => resource.decidim_commentable_id,
+          translated_column_name(:commentable_type, "decidim.comments.exports.column_name.comments") => resource.decidim_commentable_type,
+          translated_column_name(:root_commentable_url, "decidim.comments.exports.column_name.comments") => root_commentable_url
         }
       end
 
