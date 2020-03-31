9c00402f55bbbfd180183fdd017b7d28dbe6c524
quentinchampenois <quentin.champenois7@gmail.com>
2020-03-31 10:17:53 +0200

    add tests and update migration

--- decidim-assemblies/db/migrate/20200330143503_add_sort_children_option_to_assembly.rb
+++ decidim-assemblies/db/migrate/20200330143503_add_sort_children_option_to_assembly.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class AddSortChildrenOptionToAssembly < ActiveRecord::Migration[5.2]
   def change
-    add_column :assemblies, :sort_children, :boolean, default: false
+    add_column :decidim_assemblies, :sort_children, :boolean, default: false
   end
 end
