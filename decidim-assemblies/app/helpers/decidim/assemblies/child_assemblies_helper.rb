8bd17f7466ca59d804854e6be32e5724594ea48f
Quentin Champ <26109239+Quentinchampenois@users.noreply.github.com>
2020-04-06 14:08:50 +0200

    Update child_assemblies_collection
    Update this method according to the review suggestion
    
    Co-Authored-By: Armand Fardeau <armandfardeau@users.noreply.github.com>

--- decidim-assemblies/app/helpers/decidim/assemblies/child_assemblies_helper.rb
+++ decidim-assemblies/app/helpers/decidim/assemblies/child_assemblies_helper.rb
@@ -7,7 +7,8 @@ module Decidim
       # Allows to sort the child assemblies by column if needed.
       # Returns : ActiveRecord::AssociationRelation of Assemblies or empty
       def child_assemblies_collection(current_participatory_space, sorted_by = nil)
-        current_participatory_space.children.published unless current_participatory_space.sort_children?
+        return current_participatory_space.children.published unless current_participatory_space.sort_children?
+        
         current_participatory_space.children.published.sort_children_by(sorted_by)
       end
     end
