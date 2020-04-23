94b38020e5e7d499cb83d673a742a13fdf541f14
Armand Fardeau <fardeauarmand@gmail.com>
2020-04-23 19:38:15 +0200

    Add registration metadata to form

--- decidim-core/app/controllers/decidim/devise/invitations_controller.rb
+++ decidim-core/app/controllers/decidim/devise/invitations_controller.rb
@@ -6,6 +6,7 @@ module Decidim
     class InvitationsController < ::Devise::InvitationsController
       include Decidim::DeviseControllers
       include NeedsTosAccepted
+      helper_method :residential_area_for_select, :work_area_for_select, :gender_types_for_select, :month_names_for_select, :year_for_select
 
       before_action :configure_permitted_parameters
 
@@ -28,14 +29,76 @@ module Decidim
         resource.update!(newsletter_notifications_at: Time.current) if update_resource_params[:newsletter_notifications]
         resource.update!(managed: false) if resource.managed?
         resource.update!(accepted_tos_version: resource.organization.tos_version)
+        resource.update!(registration_metadata: registration_metadata(update_resource_params[:registration_metadata]))
         Decidim::Gamification.increment_score(resource.invited_by, :invitations) if resource.invited_by
         resource
+      end
+
+      private
+
+      def registration_metadata(param)
+        {
+          work_area: param[:work_area],
+          gender: param[:work_area],
+          residential_area: param[:work_area],
+          statutory_representative_email: statutory_representative_email(param),
+          birth_date: birth_date(param)
+        }
+      end
+
+      def birth_date(param)
+        {
+          month: param[:birth_date][:month],
+          year: param[:birth_date][:year]
+        }
+      end
+
+      def statutory_representative_email(param)
+        param[:statutory_representative_email] if param[:underage].present?
       end
 
       protected
 
       def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:accept_invitation, keys: [:nickname, :tos_agreement, :newsletter_notifications])
+        devise_parameter_sanitizer.permit(:accept_invitation, keys: [:nickname, :tos_agreement, :newsletter_notifications, registration_metadata: {}])
+      end
+
+      GENDER_TYPES = %w(male female other).freeze
+
+      MONTHNAMES = (1..12).map { |m| Date::MONTHNAMES[m] }.freeze
+
+      def residential_area_for_select
+        scopes
+      end
+
+      def work_area_for_select
+        scopes
+      end
+
+      def gender_types_for_select
+        GENDER_TYPES.map do |type|
+          [
+            I18n.t(type.downcase, scope: "decidim.devise.invitations.edit.gender"),
+            type
+          ]
+        end
+      end
+
+      def month_names_for_select
+        MONTHNAMES.map do |month_name|
+          [
+            I18n.t(month_name.downcase, scope: "decidim.devise.invitations.edit.month_name"),
+            month_name
+          ]
+        end
+      end
+
+      def year_for_select
+        (Time.current.year - 100..Time.current.year).map(&:to_s).reverse
+      end
+
+      def scopes
+        current_organization.scopes
       end
     end
   end
