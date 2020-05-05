ab1024efc529626d606f644059f072323c8d796f
quentinchampenois <quentin.champenois7@gmail.com>
2020-05-05 14:42:33 +0200

    remove duplicated 'metadata' method declaration

--- decidim-core/app/models/decidim/authorization.rb
+++ decidim-core/app/models/decidim/authorization.rb
@@ -16,8 +16,6 @@ module Decidim
     belongs_to :user, foreign_key: "decidim_user_id", class_name: "Decidim::User"
     has_one :organization, through: :user, class_name: "Decidim::Organization"
 
-    attr_reader :metadata
-
     validates :name, uniqueness: { scope: :decidim_user_id }
     validates :verification_metadata, absence: true, if: :granted?
     validates :verification_attachment, absence: true, if: :granted?
