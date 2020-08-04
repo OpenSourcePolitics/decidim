0917591dd86000a70d176caf46dc06484814f8e3
Quentin Champ <26109239+Quentinchampenois@users.noreply.github.com>
2020-08-04 11:07:55 +0200

    Feature rake destroy accounts (#1020)
    * create user task with env variable
    
    * destroys account logic
    
    * manage invitation_sent_at
    
    * add more specific account search
    
    * clean ENV variable from rake command
    
    * ensure env variable is not nil before delete
    
    * add a resume message when the command is over
    
    * refactor rake command

---
+++ decidim-core/lib/tasks/decidim_user_tasks.rake
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+namespace :decidim do
+  namespace :user do
+    # destroy_accounts ------
+    #
+    # Destroy user accounts using destroy_account decidim's command.
+    # Account concerned are those which seems not to sign again on platform.
+    # This command is usefull when administrator needs to remove user accounts that have been collected from old platform but don't connect again
+    desc "Destroy user accounts using decidim destroy_account command"
+
+    task destroy_accounts: :environment do |_task, _args|
+      users = Decidim::User.not_confirmed
+                           .not_deleted
+                           .where(admin: false) # user is not admin
+                           .where("invitation_sent_at <= ?", invitation_sent_date_limit) # Do not destroy accounts invited during the current month
+                           .where(invitation_accepted_at: nil) # Account must have a pending acceptation
+                           .where.not(invitation_token: nil) # Account must have a pending invitation token
+                           .where(last_sign_in_at: nil) # Account have to never been connected on platform
+
+      raise ArgumentError unless ENV["RAILS_FORCE"] == "true"
+
+      destroy_account_form = Decidim::DeleteAccountForm.new(delete_reason: "Supprimé par l'administration pour des raisons RGPD")
+
+      users.each do |user|
+        Decidim::DestroyAccount.new(user, destroy_account_form).call
+      end
+
+      puts "#{users.count} accounts have been successfully destroyed"
+    rescue ArgumentError => e
+      puts "#{users.count} accounts will be destroyed"
+      puts "#{e} : Please run task with RAILS_FORCE='true' env variable to ensure your choice"
+      puts "-----"
+      puts "OPTIONAL : Use DATE_LIMIT to redefine invitation date limit"
+      puts "Format: DATE_LIMIT='yyyy-mm-dd'"
+    ensure
+      ENV.delete "RAILS_FORCE"
+      ENV.delete "DATE_LIMIT" if ENV["DATE_LIMIT"].present?
+    end
+  end
+end
+
+def invitation_sent_date_limit
+  if ENV["DATE_LIMIT"]
+    return ENV["DATE_LIMIT"].to_date if ENV["DATE_LIMIT"].to_date.is_a? Date
+  end
+
+  Date.current - 1.month
+end
---
+++ decidim-core/spec/tasks/decidim_tasks_user_destroy_spec.rb
@@ -0,0 +1,145 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "support/tasks"
+
+describe "Executing Decidim User tasks" do
+  describe "rake decidim:user:destroy_accounts", type: :task do
+    let!(:organizations) { create_list(:organization, 2) }
+    let(:task_name) { :"decidim:user:destroy_accounts" }
+    let(:argument_error_output) { /ArgumentError : Please run task with RAILS_FORCE='true' env variable to ensure your choice/ }
+
+    shared_examples_for "have to rescue error" do |error, output|
+      it "error rescued : #{error}" do
+        Rake::Task[task_name].reenable
+        expect { Rake::Task[task_name].invoke }.to output(output).to_stdout
+      end
+    end
+
+    context "when executing task" do
+      context "without env variable" do
+        it_behaves_like"have to rescue error", "ArgumentError", /ArgumentError : Please run task with RAILS_FORCE='true' env variable to ensure your choice/
+      end
+
+      context "with env variable" do
+        context "when RAILS_FORCE has a valid content" do
+          before do
+            ENV["RAILS_FORCE"] = "true"
+          end
+
+          it "have to be executed without failures" do
+            Rake::Task[task_name].reenable
+            expect { Rake::Task[task_name].invoke }.not_to output(argument_error_output).to_stdout
+          end
+
+          context "when deletes accounts" do
+            let!(:organization) { create(:organization) }
+            let!(:admin) { create(:user, :confirmed, :admin, organization: organization) }
+            let!(:confirmed_user) { create(:user, :confirmed, organization: organization) }
+            let!(:confirmed_deleted_user) { create(:user, :confirmed, :deleted, organization: organization) }
+            let(:users) { build_list(:user, 5, organization: organization) }
+
+            let(:form) do
+              Decidim::InviteUserForm.from_params(
+                name: "Old man",
+                email: "oldman@email.com",
+                organization: organization,
+                role: "",
+                invited_by: admin,
+                invitation_instructions: "invite_user"
+              )
+            end
+
+            before do
+              users.each do |user|
+                Decidim::InviteUser.new(Decidim::InviteUserForm.from_params(
+                                          name: user.name,
+                                          email: user.email,
+                                          organization: user.organization,
+                                          role: "user_manager",
+                                          invited_by: admin,
+                                          invitation_instructions: "invite_user"
+                                        )).call
+              end
+            end
+
+            it "destroys the user account" do
+              expect(Decidim::User.where(delete_reason: nil).count).to eq(8)
+              Rake::Task[task_name].reenable
+
+              Rake::Task[task_name].invoke
+              expect(Decidim::User.where.not(delete_reason: nil).count).to eq(0)
+            end
+
+            context "when invitation_sent_at is older or equal to 1 month" do
+              before do
+                Decidim::User.all[5].update! invitation_sent_at: Date.current - 1.month - 1.day
+                Decidim::User.last.update! invitation_sent_at: Date.current - 1.month
+              end
+
+              it "destroys account" do
+                expect(Decidim::User.where(delete_reason: nil).count).to eq(8)
+                Rake::Task[task_name].reenable
+
+                Rake::Task[task_name].invoke
+                expect(Decidim::User.where.not(delete_reason: nil).count).to eq(2)
+              end
+            end
+
+            context "when invitation_sent_at is newer than 1 month" do
+              before do
+                Decidim::User.last.update! invitation_sent_at: Time.current - 29.days
+              end
+
+              it "does not destroy account" do
+                expect(Decidim::User.where(delete_reason: nil).count).to eq(8)
+                Rake::Task[:"decidim:user:destroy_accounts"].reenable
+
+                Rake::Task[task_name].invoke
+                expect(Decidim::User.where.not(delete_reason: nil).count).to eq(0)
+              end
+            end
+
+            context "when using ENV variable to define invitation_sent_at date limit" do
+              before do
+                ENV["DATE_LIMIT"] = "2020-03-01"
+                Decidim::User.last.update! invitation_sent_at: Date.current - 1.year
+              end
+
+              it "destroys unique account" do
+                expect(Decidim::User.where(delete_reason: nil).count).to eq(8)
+                Rake::Task[task_name].reenable
+
+                Rake::Task[task_name].invoke
+                expect(Decidim::User.where.not(delete_reason: nil).count).to eq(1)
+              end
+            end
+          end
+        end
+
+        context "when RAILS_FORCE has not a valid content" do
+          before do
+            ENV["RAILS_FORCE"] = "abc"
+          end
+
+          it_behaves_like "have to rescue error", "ArgumentError", /ArgumentError : Please run task with RAILS_FORCE='true' env variable to ensure your choice/
+        end
+
+        context "when rake command ends" do
+          before do
+            ENV["DATE_LIMIT"] = "2020-06-03"
+
+            Rake::Task[:"decidim:user:destroy_accounts"].reenable
+
+            Rake::Task[task_name].invoke
+          end
+
+          it "ENV variables are deleted" do
+            expect(ENV["RAILS_FORCE"]).to be_nil
+            expect(ENV["DATE_LIMIT"]).to be_nil
+          end
+        end
+      end
+    end
+  end
+end
