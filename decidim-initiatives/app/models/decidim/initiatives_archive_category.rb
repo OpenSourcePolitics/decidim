672a09ecf906779c36998cd984ed92fcaaa2ef5e
Armand Fardeau <fardeauarmand@gmail.com>
2021-04-22 14:45:27 +0200

    Remove useless has_many

--- decidim-initiatives/app/models/decidim/initiatives_archive_category.rb
+++ decidim-initiatives/app/models/decidim/initiatives_archive_category.rb
@@ -4,10 +4,6 @@ module Decidim
   class InitiativesArchiveCategory < ApplicationRecord
     belongs_to :organization, foreign_key: :decidim_organization_id, class_name: "Decidim::Organization"
 
-    has_many :initiatives,
-             class_name: "Decidim::Initiative",
-             dependent: :nullify
-
     validates :name, presence: true
     validates :name, uniqueness: { scope: :organization }
   end
