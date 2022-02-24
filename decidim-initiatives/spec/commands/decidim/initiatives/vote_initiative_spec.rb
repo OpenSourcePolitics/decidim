c4bfd3fed0aa3b9a8e9b0e5db20ab3d6f31ba094
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2022-02-24 11:35:59 +0100

    update vote initiative spec

--- decidim-initiatives/spec/commands/decidim/initiatives/vote_initiative_spec.rb
+++ decidim-initiatives/spec/commands/decidim/initiatives/vote_initiative_spec.rb
@@ -52,7 +52,7 @@ module Decidim
           expect do
             command.call
             initiative.reload
-          end.to change(initiative, :online_votes_count).by(1)
+          end.to change(initiative, :initiatives_votes_count).from(nil).to(1)
         end
 
         it "notifies the creation" do
