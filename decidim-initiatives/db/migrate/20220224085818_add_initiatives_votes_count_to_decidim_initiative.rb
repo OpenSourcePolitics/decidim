e14f553acae33386043e09a2975bcaf187959b10
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2022-02-28 16:11:11 +0100

    Refactor migration initiatives votes count

--- decidim-initiatives/db/migrate/20220224085818_add_initiatives_votes_count_to_decidim_initiative.rb
+++ decidim-initiatives/db/migrate/20220224085818_add_initiatives_votes_count_to_decidim_initiative.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
 class AddInitiativesVotesCountToDecidimInitiative < ActiveRecord::Migration[5.2]
-  def change
+  def up
     add_column :decidim_initiatives, :initiatives_votes_count, :integer
 
     Decidim::Initiative.find_each do |initiative|
       Decidim::Initiative.reset_counters(initiative.id, :votes)
     end
+  end
+
+  def down
+    remove_column :decidim_initiatives, :initiatives_votes_count, :integer
   end
 end
