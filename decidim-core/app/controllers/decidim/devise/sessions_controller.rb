a3611cac688db2a6e5d2d8bbfa7c79346b031bb4
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2022-04-07 15:27:03 +0200

    Skip authorization handler by default

--- decidim-core/app/controllers/decidim/devise/sessions_controller.rb
+++ decidim-core/app/controllers/decidim/devise/sessions_controller.rb
@@ -22,7 +22,7 @@ module Decidim
       def after_sign_in_path_for(user)
         if user.present? && user.blocked?
           check_user_block_status(user)
-        elsif first_login_and_not_authorized?(user) && !user.admin? && !pending_redirect?(user)
+        elsif first_login_and_not_authorized?(user) && !user.admin? && !pending_redirect?(user) && !skip_authorization_handler?
           decidim_verifications.first_login_authorizations_path
         else
           super
@@ -46,6 +46,11 @@ module Decidim
       end
 
       private
+
+      # Skip authorization handler by default
+      def skip_authorization_handler?
+        ENV["SKIP_FIRST_LOGIN_AUTHORIZATION"] ? ActiveRecord::Type::Boolean.new.cast(ENV["SKIP_FIRST_LOGIN_AUTHORIZATION"]) : true
+      end
 
       def check_sign_in_enabled
         redirect_to new_user_session_path unless current_organization.sign_in_enabled?
