d9059e86513ac2c6259d641fe634812ecdba2e47
stephanie rousset <strabon@club-internet.fr>
2025-02-17 16:41:48 +0100

    feat: update notify_followers method in command

--- decidim-admin/app/commands/decidim/admin/create_attachment.rb
+++ decidim-admin/app/commands/decidim/admin/create_attachment.rb
@@ -58,12 +58,15 @@ module Decidim
 
       def notify_followers
         return unless @attachment.attached_to.is_a?(Decidim::Followable)
+        return unless form.send_notification_to_followers
 
         Decidim::EventsManager.publish(
           event: "decidim.events.attachments.attachment_created",
           event_class: Decidim::AttachmentCreatedEvent,
           resource: @attachment,
-          followers: @attachment.attached_to.followers
+          followers: @attachment.attached_to.followers,
+          extra: { force_email: true },
+          force_send: true
         )
       end
 
